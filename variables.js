45dcca3e952047c02fcdd9cbdb0bea40d3d66d6b
Your Name <rzhyl05@gmail.com>
2024-09-13 16:21:38 +0800

    your commit message

---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = "Rzhyl Greta Ramos";
+console.log("Name:", name);
+
+let whatDoYouWannaBecomeInYourLife = "programmer";
+console.log("What do you want to become in your life?", whatDoYouWannaBecomeInYourLife);
+
+let gender = "FEMALE";
+console.log("gender:", gender);
+
+let twitterHandle = "@Rzhylgreta24";
+console.log("TwitterHandle:", twitterHandle);  
